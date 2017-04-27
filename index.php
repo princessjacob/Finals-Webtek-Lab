97f1e31ab2df63205021fc93da6d10cdb5c8d8ff
KristineViloria <2151386@slu.edu.ph>
2017-04-27 11:09:43 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Pet Mo, Vet Ko!</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Welcome to Pet Mo Vet Ko!";
+        ?>
+
+    </body>
+</html>
