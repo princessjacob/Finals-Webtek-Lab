64c09349656230376ea23211e55871f8d1aef4ef
KristineViloria <2151386@slu.edu.ph>
2017-04-27 11:57:38 +0800

    added dashboard.php

---
+++ Playground/Admin/dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Dashboard - Pet Mo, Vet Ko!</title>
+    </head>
+    <body>
+		<h1> Welcome back, admin 
+			<?php 
+				echo $_GET["name"];
+			?>
+		</h1>        
+    </body>
+</html>
