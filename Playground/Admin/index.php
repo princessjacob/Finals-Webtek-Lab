2a0247770fb1b7d90a8d8ece26f54993f2d4ac6a
KristineViloria <2151386@slu.edu.ph>
2017-04-29 00:42:22 +0800

    updated index.php

--- Playground/Admin/index.php
+++ Playground/Admin/index.php
@@ -5,9 +5,27 @@
     </head>
     <body>
 
-        <?php
-            echo "Welcome to Pet Mo Vet Ko!";
-        ?>
+    	<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "petmovetko";
 
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn->connect_error) {
+    			die("Connection failed: " . $conn->connect_error);
+			} 
+			echo "Connected successfully";
+		?>
+
+        <h1> Welcome to Pet Mo, Vet Ko! </h1>
+		<form action="actions\authentication.php" method="get">
+  			Email: <input type="text" name="name"><br>
+  			Password: <input type="password" name="pass"><br>
+  			<input class="login-button" type="submit" value="Login">
+		</form>
+			<input class="signup-button" type="submit" value="Create Account" onclick="window.location='/pages/signup';">
     </body>
 </html>
