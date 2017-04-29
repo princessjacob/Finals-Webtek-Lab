96f87ecf7fc2d86db0314cd286b3e42a2fbc832e
KristineViloria <2151386@slu.edu.ph>
2017-04-30 00:26:06 +0800

    modified index.php

--- Playground/Admin/index.php
+++ Playground/Admin/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
 
-    	<?php
+    <?php
 			$servername = "localhost";
 			$username = "root";
 			$password = "";
@@ -21,10 +21,10 @@
 
 		<header>
 		  <form action="actions\authentication.php" method="get">
-  		  <input type="text" name="name" value="Email"
+  		  <input type="text" name="email-signin" value="Email"
           onblur="if (this.value=='') {this.value='Email';}" 
           onfocus="if (this.value == 'Email') {this.value='';}">
-        <input type="text" name="name" value="Password"
+        <input type="text" name="pass-signin" value="Password"
           onblur="if (this.value=='') {this.value='Password';}" 
           onfocus="if (this.value == 'Password') {this.value='';}">
   			 <input class="login-button" type="submit" value="Login">
@@ -38,31 +38,67 @@
 		<h2> Care for your pets now! </h2>
 		<div> <p> With professionals always in the work, you never have to worry about the circumstance of your pet.</p></div>
 
-    <!--Delete hr after putting css-->
     <hr>
 
-    <div id="sign-up-form">
-		  <form action="actions/createaccount.php" method="get">
-        <h3> Name </h3>
-  			<input type="text" name="firstName" value="First Name"
-          onblur="if (this.value=='') {this.value='First Name';}" 
-          onfocus="if (this.value == 'First Name') {this.value='';}">
-  			<input type="text" name="lastName" value="Last Name"
-          onblur="if (this.value=='') {this.value='Last Name';}" 
-          onfocus="if (this.value == 'Last Name') {this.value='';}"><br>
-        <h3> Contact </h3>
-        Contact Number: <input type="text" name="address"><br>
-  			Email: <input type="text" name="email"><br>
-  			Password: <input type="password" name="pass"><br>
-  			Street Address: <input type="text" name="address"><br>
-        City/Province: <input type="text" name="address"><br>
-        Region: <input type="text" name="address"><br>
-        Country: <input type="text" name="address"><br>
-  			<input class="create-account" type="submit" value="Create Account"><br>
-        <a href="pages/register.php">I am a Service Provider</a>
-		  </form>
-    </div>
+	  <form action="<?php $PHP_SELF; ?>" method="POST">
+      <h3> Name </h3>
+ 			<input type="text" name="firstName" value="First Name"
+        onblur="if (this.value=='') {this.value='First Name';}" 
+        onfocus="if (this.value == 'First Name') {this.value='';}">
+			<input type="text" name="lastName" value="Last Name"
+        onblur="if (this.value=='') {this.value='Last Name';}" 
+        onfocus="if (this.value == 'Last Name') {this.value='';}"><br>
+      <h3> Contact </h3>
+      Contact Number: <input type="text" name="contactno"><br>
+			Email: <input type="text" name="email"><br>
+ 			Password: <input type="password" name="pass"><br>
+      Home Address: <input type="text" name="homeadd"><br>
+ 			Street Address: <input type="text" name="streetadd"><br>
+      City/Province: <input type="text" name="cityprovince"><br>
+      Region: <input type="text" name="region"><br>
+      Country: <input type="text" name="country"><br>
+			<input class="create-account" type="submit" name="customerregister" value="Create Account"><br>
+    </form>
 
 
-    </body>
+    <!-- PHP SCRIPT for FORM -->
+    
+    <?php
+      if(isset($_POST["customerregister"])) {
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $email = $_POST["email"];
+        $homeadd = $_POST["homeadd"];
+        $pass = $_POST["pass"];
+        $streetadd = $_POST["streetadd"];
+        $cityprovince = $_POST["cityprovince"];
+        $region = $_POST["region"];
+        $country = $_POST["country"];
+        $contactno = $_POST["contactno"];
+
+        $result = mysqli_query($conn, "SELECT * FROM customers WHERE Email = '$email'");
+
+
+
+        if(mysqli_num_rows($result) > 0) {
+          print "Account already exists!";
+          mysqli_free_result($result);
+        } else {
+          $sql = "INSERT INTO Customers (LastName, FirstName, Email, Password, HomeAdd, StreetAdd, CityProvince, Region, Country, ContactNo) VALUES ('$lastName', '$firstName', '$email', '$pass', '$homeadd', '$streetadd', '$cityprovince', '$region', '$country', '$contactno')";
+
+          if ($conn->query($sql) === TRUE) {
+            mysqli_free_result($result);
+            header("pages/admindashboard.php");
+
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+        }
+      }   
+    ?>
+
+    <br>
+
+    <a href="pages/register.php">I am a Service Provider</a>
+  </body>
 </html>
