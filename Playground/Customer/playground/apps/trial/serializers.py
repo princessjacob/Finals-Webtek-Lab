b1ce59ed490ccd0d10f215eaa650fad799007015
Princess B. Jacob <2151809@slu.edu.ph>
2017-04-30 21:20:40 +0800

    Added playground trial for a django project (Trial the First)
    - Created a class that goes on with Model, Views and Serializer

---
+++ Playground/Customer/playground/apps/trial/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Trial
+
+class TrialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trial
+        fields = '__all__'
