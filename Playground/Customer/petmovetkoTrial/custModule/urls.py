1a1bdf1dc7cbb1c4a945ba8e9a1068d579486a0d
Princess B. Jacob <2151809@slu.edu.ph>
2017-05-04 01:32:29 +0800

    Updated urls and views to allow other parts of the navbar to work.

--- Playground/Customer/petmovetkoTrial/custModule/urls.py
+++ Playground/Customer/petmovetkoTrial/custModule/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^request/$', views.request, name="request"),
+    url(r'^appointment/$', views.appointment, name="appointment"),
+    url(r'^transaction/$', views.transaction, name="transaction"),
+    url(r'^review/$', views.review, name="review"),
+    url(r'^profile/$', views.profile, name="profile"),
 ]
--- Playground/Customer/petmovetkoTrial/custModule/views.py
+++ Playground/Customer/petmovetkoTrial/custModule/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'custModule/index.html')
+def dashboard(request):
+    return render(request, 'custModule/dashboard.html')
+
+def appointment(request):
+    return render(request, 'custModule/appointment.html')
+
+def request(request):
+    return render(request, 'custModule/request.html')
+
+def review(request):
+    return render(request, 'custModule/review.html')
+
+def transaction(request):
+    return render(request, 'custModule/transaction.html')
+
+def profile(request):
+    return render(request, 'custModule/profile.html')
+
