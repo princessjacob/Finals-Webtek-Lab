9c0da2520b402f2a988db2333e7ace54701ca4a1
Princess B. Jacob <2151809@slu.edu.ph>
2017-05-04 01:38:23 +0800

    Updated urls.py to make all pages of the site work.

--- Playground/Customer/petmovetkoTrial/config/urls.py
+++ Playground/Customer/petmovetkoTrial/config/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^$', include('custModule.urls')),
+    url(r'^', include('custModule.urls')),
+    url(r'^trial/', include('trial.urls')),
 ]
