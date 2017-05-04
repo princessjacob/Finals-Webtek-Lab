6167e23d804ce363e3cf25f13e343145051d98f3
iamanthony401 <iamanthony401@gmail.com>
2017-05-04 19:05:40 +0800

    Modified the register.php by adding the hash (md5) function in password

--- Playground/Admin/register.php
+++ Playground/Admin/register.php
@@ -9,15 +9,12 @@
 		$username = "root";
 		$password = "";
 		$dbname = "petmovetko";
-
 		$petmovetkodb = new mysqli("localhost", "root", "", "petmovetko");
 		$worlddb = new mysqli("localhost", "root", "", "world");
-
 		//Check connection
 		if ($petmovetkodb->connect_error) {
    			die("Connection failed: " . $petmovetkodb->connect_error);
 		}
-
 		if ($worlddb->connect_error) {
    			die("Connection failed: " . $worlddb->connect_error);
 		}
@@ -28,7 +25,6 @@
 			$('#')
 		}
 	</script>
-
 	<h1> Greetings, Service Provider! </h1>
 	<h2> Fill out the form to register your service </h2>
 	<form action="register.php" method="POST">
@@ -55,7 +51,6 @@
         		$countryoption = mysqli_query($worlddb,"SELECT Name FROM country");
         		while($row=mysqli_fetch_array($countryoption,MYSQLI_ASSOC)) {
         			$countryname = $row['Name'];
-
         			echo "<option value='$countryname'> $countryname </option>";
         		}
         	?>
@@ -71,61 +66,46 @@
         	onfocus="if (this.value == 'Barangay') {this.value='';}" required><br>
 		
 		<h3> Service Information </h3>
-
 		<span style="font-weight: bold; font-size: 20;"> I can care for: </span>
 		<input type="checkbox" name="petType" value="cat"> Cats
 		<input type="checkbox" name="petType" value="dog"> Dogs <br>
-
 		Services:
 		<select name="services">
 		</select>
-
 		
 		<br>
-
-
 		<input type="submit" name="spregister" 	value="Register"><br>
-
-
-
     </form>
-
     <!-- REGISTRATION ACTION -->
     
     <?php
       if(isset($_POST["spregister"])) {
-
         $firstName = $_POST["firstName"];
         $lastName = $_POST["lastName"];
         $title = $_POST["title"];
         $email = $_POST["email"];
         $pass = $_POST["pass"];
+	$pass = md5($pass);s
         $barangay = $_POST["barangay"];
         $region = $_POST["region"];
         $cityprovince = $_POST["cityprovince"];
         $country = $_POST["country"];
         $contactno = $_POST["contactno"];
-
         if (empty($firstName) || empty($lastName) || empty($email) || empty($pass) || empty($streetadd) || empty($barangay) || empty($cityprovince) ||  empty($region) || empty($country) || empty($contactno)) {
-
           echo "Please fill up all the parts of the form";
-
         } else if (!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email)) {
           echo "The email you have entered is invalid, please try again.";
         } else {
           $result = mysqli_query($conn, "SELECT * FROM customers WHERE Email = '$email'");
-
           if(mysqli_num_rows($result) > 0) {
             print "Account already exists!";
             mysqli_free_result($result);
           } else {
             $sql = "INSERT INTO Customers (LastName, FirstName, Email, Password, StreetAdd, Barangay, CityProvince, Region, Country, ContactNo) VALUES ('$lastName', '$firstName', '$email', '$pass', '$streetadd', '$barangay', '$cityprovince', '$region', '$country', '$contactno')";
-
             if ($conn->query($sql) === TRUE) {
               $_SESSION['loggedin'] = true;
               $_SESSION['user'] = $email;
               header("Location: pages/accountcreated.php");
-
             } else {
               echo "Error: " . $sql . "<br>" . $conn->error;
             }
@@ -133,7 +113,6 @@
         }
       }   
     ?>
-
     <!-- End of SIGN UP ACTION -->
     </body>
 </html>
