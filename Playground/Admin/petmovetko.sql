046efc03d68d20314879b497939683e5b0dc36c4
MarlaZitaCSP <2115764@slu.edu.ph>
2017-05-04 23:06:58 +0800

    FINAL DATABASE

--- Playground/Admin/petmovetko.sql
+++ Playground/Admin/petmovetko.sql
@@ -23,14 +23,15 @@ DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customer` (
-  `custID` int(11) NOT NULL,
+  `custID` int(11) NOT NULL AUTO_INCREMENT,
   `custName` varchar(45) NOT NULL,
   `custAdd` varchar(45) NOT NULL,
   `custNum` int(11) NOT NULL,
   `custEmail` varchar(45) NOT NULL,
+  PRIMARY KEY (`custID`),
   KEY `rr_ID_idx` (`custID`),
-  CONSTRAINT `reqID` FOREIGN KEY (`custID`) REFERENCES `request` (`reqID`) ON UPDATE CASCADE,
-  CONSTRAINT `rr_ID` FOREIGN KEY (`custID`) REFERENCES `review_rating` (`rr_ID`) ON UPDATE CASCADE
+  CONSTRAINT `customer.reqID` FOREIGN KEY (`custID`) REFERENCES `request` (`reqID`) ON UPDATE CASCADE,
+  CONSTRAINT `customer.servID` FOREIGN KEY (`custID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -51,7 +52,7 @@ DROP TABLE IF EXISTS `request`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `request` (
-  `reqID` int(11) NOT NULL,
+  `reqID` int(11) NOT NULL AUTO_INCREMENT,
   `reqStatus` varchar(45) NOT NULL,
   `reqDets` varchar(45) NOT NULL,
   PRIMARY KEY (`reqID`)
@@ -68,27 +69,29 @@ LOCK TABLES `request` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `review_rating`
+-- Table structure for table `reviewrating`
 --
 
-DROP TABLE IF EXISTS `review_rating`;
+DROP TABLE IF EXISTS `reviewrating`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `review_rating` (
+CREATE TABLE `reviewrating` (
   `rr_ID` int(11) NOT NULL,
   `revDets` varchar(45) NOT NULL,
   `rating` int(11) NOT NULL,
-  PRIMARY KEY (`rr_ID`)
+  PRIMARY KEY (`rr_ID`),
+  CONSTRAINT `revrat.custID` FOREIGN KEY (`rr_ID`) REFERENCES `customer` (`custID`) ON UPDATE CASCADE,
+  CONSTRAINT `revrat.spID` FOREIGN KEY (`rr_ID`) REFERENCES `service_provider` (`spID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `review_rating`
+-- Dumping data for table `reviewrating`
 --
 
-LOCK TABLES `review_rating` WRITE;
-/*!40000 ALTER TABLE `review_rating` DISABLE KEYS */;
-/*!40000 ALTER TABLE `review_rating` ENABLE KEYS */;
+LOCK TABLES `reviewrating` WRITE;
+/*!40000 ALTER TABLE `reviewrating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reviewrating` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -99,14 +102,16 @@ DROP TABLE IF EXISTS `service_provider`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `service_provider` (
-  `spID` int(11) NOT NULL,
+  `spID` int(11) NOT NULL AUTO_INCREMENT,
   `spName` varchar(45) NOT NULL,
   `spAdd` varchar(45) NOT NULL,
   `spNum` int(11) NOT NULL,
   `spEmail` varchar(45) NOT NULL,
   `spStatus` varchar(45) NOT NULL,
   PRIMARY KEY (`spID`),
-  CONSTRAINT `serv_ID` FOREIGN KEY (`spID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE
+  CONSTRAINT `servprov.reqID` FOREIGN KEY (`spID`) REFERENCES `request` (`reqID`) ON UPDATE CASCADE,
+  CONSTRAINT `servprov.servID` FOREIGN KEY (`spID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE,
+  CONSTRAINT `servprov.transID` FOREIGN KEY (`spID`) REFERENCES `transaction` (`trans_ID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -127,11 +132,15 @@ DROP TABLE IF EXISTS `services`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `services` (
-  `servID` int(11) NOT NULL,
+  `servID` int(11) NOT NULL AUTO_INCREMENT,
   `servName` varchar(45) NOT NULL,
   `servDesc` varchar(45) NOT NULL,
   `servPrice` int(11) NOT NULL,
-  PRIMARY KEY (`servID`)
+  PRIMARY KEY (`servID`),
+  UNIQUE KEY `servID_UNIQUE` (`servID`),
+  CONSTRAINT `service.custID` FOREIGN KEY (`servID`) REFERENCES `customer` (`custID`) ON UPDATE CASCADE,
+  CONSTRAINT `service.spID` FOREIGN KEY (`servID`) REFERENCES `service_provider` (`spID`) ON UPDATE CASCADE,
+  CONSTRAINT `service.transID` FOREIGN KEY (`servID`) REFERENCES `transaction` (`trans_ID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -142,6 +151,31 @@ CREATE TABLE `services` (
 LOCK TABLES `services` WRITE;
 /*!40000 ALTER TABLE `services` DISABLE KEYS */;
 /*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ssp`
+--
+
+DROP TABLE IF EXISTS `ssp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ssp` (
+  `servID` int(11) NOT NULL,
+  `spID` int(11) NOT NULL,
+  PRIMARY KEY (`servID`,`spID`),
+  CONSTRAINT `ssp.servID` FOREIGN KEY (`servID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE,
+  CONSTRAINT `ssp.spID` FOREIGN KEY (`servID`) REFERENCES `service_provider` (`spID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ssp`
+--
+
+LOCK TABLES `ssp` WRITE;
+/*!40000 ALTER TABLE `ssp` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ssp` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -160,9 +194,10 @@ CREATE TABLE `transaction` (
   `payment` int(11) NOT NULL,
   `payStatus` varchar(45) NOT NULL,
   PRIMARY KEY (`trans_ID`),
-  CONSTRAINT `custID` FOREIGN KEY (`trans_ID`) REFERENCES `customer` (`custID`) ON UPDATE CASCADE,
-  CONSTRAINT `servID` FOREIGN KEY (`trans_ID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE,
-  CONSTRAINT `spID` FOREIGN KEY (`trans_ID`) REFERENCES `service_provider` (`spID`) ON UPDATE CASCADE
+  CONSTRAINT `transaction.custID` FOREIGN KEY (`trans_ID`) REFERENCES `customer` (`custID`) ON UPDATE CASCADE,
+  CONSTRAINT `transaction.reqID` FOREIGN KEY (`trans_ID`) REFERENCES `request` (`reqID`) ON UPDATE CASCADE,
+  CONSTRAINT `transaction.servID` FOREIGN KEY (`trans_ID`) REFERENCES `services` (`servID`) ON UPDATE CASCADE,
+  CONSTRAINT `transaction.spID` FOREIGN KEY (`trans_ID`) REFERENCES `service_provider` (`spID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -188,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-03 21:29:42
+-- Dump completed on 2017-05-04 23:06:37
