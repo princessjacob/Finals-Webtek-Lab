285360a560ca15d6f5f947173161452a5e99a813
engelyncarreon <engelyn.carreon@gmail.com>
2017-05-15 21:48:41 +0800

    Modified the functions

---
+++ Final/customer/custModuleF/custModule/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import Customer, Complaints, Request, Reviewrating, ServiceProvider, Services, Ssp, Transaction
+
+class UpdateForm(forms.ModelForm):
+    custlastname = forms.CharField(max_length=45)  
+    custfirstname = forms.CharField(max_length=45)  
+    custemail = forms.CharField(max_length=45)  
+    custadd = forms.CharField(max_length=45)  
+    custzip = forms.CharField(max_length=45)
+    custnum = forms.CharField(max_length=45)  
+    custabout = forms.CharField(max_length=1000)
+
+    class Meta:
+        model = Customer
+        exclude = ['custid', 'custpassword']
+        
+class NewRequestForm(forms.ModelForm):
+    reqstatus = forms.CharField(required=False, widget=forms.HiddenInput(), initial='pending')
+    class Meta:
+        model = Request
+        fields = '__all__'
+        
+class EditRequestForm(forms.ModelForm):
+    reqstats = forms.CharField(required=False, widget=forms.HiddenInput(), initial='pending')
+    class Meta:
+        model = Request
+        fields = '__all__'
+        
+class NewReviewForm(forms.ModelForm):
+    revmessage = forms.CharField(max_length=10000,widget=forms.Textarea(attrs={'rows': 5, 'cols': 130}))
+    rating = forms.IntegerField(min_value=1, max_value=10)
+    class Meta:
+        model = Reviewrating
+        exclude = ['rr_id', 'reviewer']
+        
+class NewComplaintForm(forms.ModelForm):
+    compmessage = forms.CharField(max_length=10000,widget=forms.Textarea(attrs={'rows': 5, 'cols': 130}))
+    class Meta:
+        model = Complaints
+        exclude = ['compid', 'complainer']
---
+++ Final/customer/custModuleF/custModule/static/vendor/ReadMe.txt
@@ -0,0 +1,1 @@
+This folder contains all the vendor type files that came in bundled with the bootstrap template used for the customer module site.
